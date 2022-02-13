315b8186ca0fec840f5f64d5d5535527e5a1b3b5
shiven237 <shivendra.pts@gmail.com>
2022-02-13 18:49:26 +0530

    coding assignment

---
+++ src/test/java/com/shiven/LogAnalyser/LogAnalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiven.LogAnalyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogAnalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
